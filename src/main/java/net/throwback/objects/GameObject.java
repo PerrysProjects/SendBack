8f4064fcc608588e70ff138bb1baceb51421dba4
Perry <kicrafter26@gmail.com>
2024-04-03 20:51:58 +0200

    Started Object changes
    Some stuff into Enum Attributes

--- src/main/java/net/throwback/objects/GameObject.java
+++ src/main/java/net/throwback/objects/GameObject.java
@@ -3,25 +3,19 @@ package net.throwback.objects;
 import net.throwback.objects.objectId.ObjectId;
 
 import java.awt.*;
-import java.awt.image.ImageObserver;
-import java.text.AttributedCharacterIterator;
 
 public abstract class GameObject {
 
     protected int x, y;
-
     protected int width, height;
-
     protected ObjectId id;
-    protected ObjectId.Image image;
 
-    public GameObject(int x, int y, int width, int height, ObjectId id, ObjectId.Image image) {
+    public GameObject(int x, int y, int width, int height, ObjectId id) {
         this.x = x;
         this.y = y;
         this.width = width;
         this.height = height;
         this.id = id;
-        this.image = image;
     }
 
 
@@ -45,192 +39,13 @@ public abstract class GameObject {
     public int getHeight() {
         return height;
     }
-    public ObjectId.Image getImage(){ return image;}
+
+    public Image getImage() {
+        return id.getImage();
+    }
 
     public ObjectId getId() {
         return id;
     }
-
-    //////////////
-    Graphics g = new Graphics() {
-        @Override
-        public Graphics create() {
-            return null;
-        }
-
-        @Override
-        public void translate(int x, int y) {
-
-        }
-
-        @Override
-        public Color getColor() {
-            return null;
-        }
-
-        @Override
-        public void setColor(Color c) {
-
-        }
-
-        @Override
-        public void setPaintMode() {
-
-        }
-
-        @Override
-        public void setXORMode(Color c1) {
-
-        }
-
-        @Override
-        public Font getFont() {
-            return null;
-        }
-
-        @Override
-        public void setFont(Font font) {
-
-        }
-
-        @Override
-        public FontMetrics getFontMetrics(Font f) {
-            return null;
-        }
-
-        @Override
-        public Rectangle getClipBounds() {
-            return null;
-        }
-
-        @Override
-        public void clipRect(int x, int y, int width, int height) {
-
-        }
-
-        @Override
-        public void setClip(int x, int y, int width, int height) {
-
-        }
-
-        @Override
-        public Shape getClip() {
-            return null;
-        }
-
-        @Override
-        public void setClip(Shape clip) {
-
-        }
-
-        @Override
-        public void copyArea(int x, int y, int width, int height, int dx, int dy) {
-
-        }
-
-        @Override
-        public void drawLine(int x1, int y1, int x2, int y2) {
-
-        }
-
-        @Override
-        public void fillRect(int x, int y, int width, int height) {
-
-        }
-
-        @Override
-        public void clearRect(int x, int y, int width, int height) {
-
-        }
-
-        @Override
-        public void drawRoundRect(int x, int y, int width, int height, int arcWidth, int arcHeight) {
-
-        }
-
-        @Override
-        public void fillRoundRect(int x, int y, int width, int height, int arcWidth, int arcHeight) {
-
-        }
-
-        @Override
-        public void drawOval(int x, int y, int width, int height) {
-
-        }
-
-        @Override
-        public void fillOval(int x, int y, int width, int height) {
-
-        }
-
-        @Override
-        public void drawArc(int x, int y, int width, int height, int startAngle, int arcAngle) {
-
-        }
-
-        @Override
-        public void fillArc(int x, int y, int width, int height, int startAngle, int arcAngle) {
-
-        }
-
-        @Override
-        public void drawPolyline(int[] xPoints, int[] yPoints, int nPoints) {
-
-        }
-
-        @Override
-        public void drawPolygon(int[] xPoints, int[] yPoints, int nPoints) {
-
-        }
-
-        @Override
-        public void fillPolygon(int[] xPoints, int[] yPoints, int nPoints) {
-
-        }
-
-        @Override
-        public void drawString(String str, int x, int y) {
-
-        }
-
-        @Override
-        public void drawString(AttributedCharacterIterator iterator, int x, int y) {
-
-        }
-
-        @Override
-        public boolean drawImage(Image img, int x, int y, ImageObserver observer) {
-            return false;
-        }
-
-        @Override
-        public boolean drawImage(Image img, int x, int y, int width, int height, ImageObserver observer) {
-            return false;
-        }
-
-        @Override
-        public boolean drawImage(Image img, int x, int y, Color bgcolor, ImageObserver observer) {
-            return false;
-        }
-
-        @Override
-        public boolean drawImage(Image img, int x, int y, int width, int height, Color bgcolor, ImageObserver observer) {
-            return false;
-        }
-
-        @Override
-        public boolean drawImage(Image img, int dx1, int dy1, int dx2, int dy2, int sx1, int sy1, int sx2, int sy2, ImageObserver observer) {
-            return false;
-        }
-
-        @Override
-        public boolean drawImage(Image img, int dx1, int dy1, int dx2, int dy2, int sx1, int sy1, int sx2, int sy2, Color bgcolor, ImageObserver observer) {
-            return false;
-        }
-
-        @Override
-        public void dispose() {
-        }
-    };
 }
 
--- src/main/java/net/throwback/objects/objectId/ObjectId.java
+++ src/main/java/net/throwback/objects/objectId/ObjectId.java
@@ -1,9 +1,24 @@
 package net.throwback.objects.objectId;
 
-public enum ObjectId {
-    Player();
+import java.awt.*;
+import java.awt.image.BufferedImage;
 
-    public enum Image {
+public enum ObjectId {
+    Player("Player", new BufferedImage(6, 6, Image.SCALE_DEFAULT));
+
+    private String name;
+    private Image image;
+
+    ObjectId(String name, Image image) {
+
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public Image getImage() {
+        return image;
     }
 }
 
