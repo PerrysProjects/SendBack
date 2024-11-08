a2ff8792e65c6d21e99886eb2b4db0c868a9db9b
Perry <105546168+PerrysProjects@users.noreply.github.com>
2024-11-08 17:03:22 +0100

    Update SessionButton.java

--- src/main/java/net/sendback/util/components/SessionButton.java
+++ src/main/java/net/sendback/util/components/SessionButton.java
@@ -18,16 +18,6 @@ public class SessionButton extends JButton implements ActionListener {
         setMinimumSize(new Dimension(150, 100));
         setMaximumSize(new Dimension(150, 100));
         setText(session.getName());
-
-        // wird noch zu Round Button
-/*
-        RoundButton sessionButton = new RoundButton(session.getName());
-        sessionButton.setFont(getFonts()[0]);
-        setSize(new Dimension(150, 100));
-        setMinimumSize(new Dimension(150, 100));
-        setMaximumSize(new Dimension(150, 100));
-        sessionButton.setPreferredSize(new Dimension(200, 200));
-       */
         
         setFocusable(true);
         addActionListener(this);
@@ -38,11 +28,6 @@ public class SessionButton extends JButton implements ActionListener {
     @Override
     public void actionPerformed(ActionEvent e) {
         GameCanvas.getInstance().setup(session);
-
-        /*Frame.getInstance().getContentPane().removeAll();
-        Frame.getInstance().add(GameCanvas.getInstance());
-        Frame.getInstance().revalidate();
-        Frame.getInstance().repaint();*/
 
         Frame.getInstance().switchPanel(GameCanvas.getInstance());
     }
